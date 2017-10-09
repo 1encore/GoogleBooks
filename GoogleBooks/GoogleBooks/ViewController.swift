064966f2ed8bb3fa810e748a35d961add380bec3
hg1zadr <hg1zadr@gmail.com>
2017-10-09 13:24:36 +0600

    Add files via upload
    Google books till 4 video

---
+++ GoogleBooks/GoogleBooks/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  GoogleBooks
+//
+//  Created by INO1 on 09.10.17.
+//  Copyright © 2017 1encore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell
+        
+        return cell;
+    }
+}
